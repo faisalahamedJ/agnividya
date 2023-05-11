93da891b20beed9bff94f7ce639463f3d6144092
faisalahamedJ <37343326+faisalahamedJ@users.noreply.github.com>
2023-05-11 19:10:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".navbar-container");
+  if (window.innerWidth > 992) {
+    navbar.classList.remove("sticky-top");
+  }
+
+  const mybutton = document.querySelector(".move-to-top");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 150
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  mybutton.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+});
