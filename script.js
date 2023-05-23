5c7c3232facaed65fa3777e292171b1aeffc0071
faisalahamedJ <37343326+faisalahamedJ@users.noreply.github.com>
2023-05-23 20:27:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,3 +35,12 @@ document.addEventListener("DOMContentLoaded", function () {
     wholeContainer.style.display = "block";
   });
 });
+
+function showPreview(event) {
+  if (event.target.files.length > 0) {
+    var src = URL.createObjectURL(event.target.files[0]);
+    var preview = document.getElementById("file-ip-1-preview");
+    preview.src = src;
+    preview.style.display = "block";
+  }
+}
